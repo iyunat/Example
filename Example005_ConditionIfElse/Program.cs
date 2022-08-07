31d035ddc74a2a1bbecc966a5e3991df2992e31a
Natalya <iyunat@yandex.ru>
2022-08-08 00:16:02 +0300

    Example 005Condition if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("ВВедите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower () == "дима")
+{
+    Console.WriteLine ("Ура, это же Дима");
+}
+else
+{
+    Console.Write ("Привет,");
+    Console.WriteLine (username);
+}
