9ff9bbfc3731f263624d99668961fca2aaed9d99
Natalya <iyunat@yandex.ru>
2022-08-14 09:17:29 +0300

    Creating Example011ArrayLibrary

--- Example010MetohdArray/Program.cs
+++ Example010MetohdArray/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int [] array ={15, 92, 34, 74, 45, 63, 74, 58};
+
+int n = array.Length;
+int find = 74;
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+   // index= index+1;
+   index++;
+}
+
+
