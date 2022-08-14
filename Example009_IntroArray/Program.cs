f24ef1c0e248dfc1d25929307585f0d5046e2901
Natalya <iyunat@yandex.ru>
2022-08-14 08:27:45 +0300

    Creating Example010MetohdArray

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {51, 25, 63, 47, 55, 96, 77, 18, 91};
+array[0] =12;
+
+Console.WriteLine(array[4]);
+
+int max = Max (
+            Max(array [0], array[1], array[2]),
+            Max (array [3], array[4], array[5]), 
+            Max (array [6], array[7], array[8]));
+
+Console.WriteLine(max);
+
+
+
